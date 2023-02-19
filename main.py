1a6f8410bc1314219427aa8322ec6de836530ad6
som-thing <81899399+som-thing@users.noreply.github.com>
2023-02-19 22:14:12 +0600

    5, 6

--- main.py
+++ main.py
@@ -2,12 +2,37 @@ import sys
 import requests
 import pygame
 import os
+import json
 
 
 pygame.init()
+apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
 spn1, spn2 = "0.01", "0.01"
 coords1, coords2 = map(str, input().split(", "))
 l = "map"
+pt = None
+
+
+def geocode(address):
+    req = f"http://geocode-maps.yandex.ru/1.x/?apikey={apikey}&geocode={address}&format=json"
+    res = requests.get(req)
+    if res:
+        json_res = res.json()
+        with open('cats_3.json', 'w') as cat_file:
+            json.dump(json_res, cat_file)
+    else:
+        raise RuntimeError(
+            """Ошибка выполнения запроса:
+            {request}
+            Http статус: {status} ({reason})""".format(
+                request=req, status=res.status_code, reason=res.reason))
+    features = json_res["response"]["GeoObjectCollection"]["featureMember"]
+    if features:
+        global spn1, spn2, coords1, coords2, pt, img
+        coords2, coords1 = features[0]["GeoObject"]["Point"]["pos"].split(" ")
+        pt = "{0},{1}".format(coords2, coords1)
+        spn1, spn2 = "0.01", "0.01"
+        img = search()
 
 
 def change_spn(flag):
@@ -37,7 +62,10 @@ def search():
     map_params = {
         'll': coords2 + ',' + coords1,
         'spn': spn1 + ',' + spn2,
-        'l': l}
+        'l': l
+    }
+    if pt:
+        map_params['pt'] = "{0},pm2dgl".format(pt)
     response = requests.get(maps_server, params=map_params)
     with open('map.png', 'wb') as f:
         f.write(response.content)
@@ -51,39 +79,70 @@ def terminate():
     sys.exit()
 
 
+font = pygame.font.Font(None, 32)
 pygame.display.set_caption('YL-MAP')
 img = search()
 size = width, height = img.get_width(), img.get_height() + 50
 screen = pygame.display.set_mode(size)
 running = True
 img = search()
+
+input_box = pygame.Rect(10, img.get_height() + 10, 400, 32)
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
+color = color_inactive
+active = False
+text = ''
+done = False
+
 FPS = 50
 clock = pygame.time.Clock()
 while running:
-    screen.blit(img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_PAGEDOWN:
-                change_spn(True)
-            elif event.key == pygame.K_PAGEUP:
-                change_spn(False)
-            elif event.key == pygame.K_UP:
-                move("up")
-            elif event.key == pygame.K_DOWN:
-                move("down")
-            elif event.key == pygame.K_LEFT:
-                move("left")
-            elif event.key == pygame.K_RIGHT:
-                move("right")
-            elif event.key == pygame.K_b:  # это 4-ое
-                l = "map"
-            elif event.key == pygame.K_n:
-                l = "sat"
-            elif event.key == pygame.K_m:
-                l = "sat,skl"
-            img = search()
+            if active:
+                if event.key == pygame.K_RETURN:
+                    geocode(text)
+                    text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+            else:
+                if event.key == pygame.K_PAGEDOWN:
+                    change_spn(True)
+                elif event.key == pygame.K_PAGEUP:
+                    change_spn(False)
+                elif event.key == pygame.K_UP:
+                    move("up")
+                elif event.key == pygame.K_DOWN:
+                    move("down")
+                elif event.key == pygame.K_LEFT:
+                    move("left")
+                elif event.key == pygame.K_RIGHT:
+                    move("right")
+                elif event.key == pygame.K_F1:
+                    l = "map"
+                elif event.key == pygame.K_F2:
+                    l = "sat"
+                elif event.key == pygame.K_F3:
+                    l = "sat,skl"
+                img = search()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_box.collidepoint(event.pos):
+                active = not active
+            else:
+                active = False
+            color = color_active if active else color_inactive
+    screen.fill((0, 0, 0))
+    screen.blit(img, (0, 0))
+    txt_surface = font.render(text, True, color)
+    width = max(400, txt_surface.get_width() + 10)
+    input_box.w = width
+    screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+    pygame.draw.rect(screen, color, input_box, 2)
     pygame.display.flip()
     clock.tick(FPS)
 pygame.quit()
